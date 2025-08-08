700c42a38916a6ac32d8efc5b19701e6d60542ad
NANDA DULAL KUMBHAKAR <153352037+nandadulalkumbhakar@users.noreply.github.com>
2025-08-08 15:27:06 +0530

    Add files via upload

---
+++ Blood Bank Management System/src/main/java/Project/ConnectionProvider.java
@@ -0,0 +1,24 @@
+package Project;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+//import java.sql.*;
+
+public class ConnectionProvider {
+	
+	public static Connection getCon()
+	{
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/bloodbank","root","password");
+			return con;
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+			return null;
+		}
+	}
+}
+
